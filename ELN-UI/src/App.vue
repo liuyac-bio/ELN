d2630c49c0bb39db21e8f29d8f3ed516eca02e15
liuyac-bio <bio@liuyac.cn>
2024-12-26 22:51:03 +0800

    v-0.0.01

---
+++ ELN-UI/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+</script>
+
+<template>
+  <!-- 2024-12-17 14:33:43  删除项目初始化生成的html-->
+  <!-- 2024-12-17 14:34:54 引入路由插槽视图 -->
+   <RouterView></RouterView>
+</template>
+
+<style scoped>
+/*
+2024-12-17 14:32:33 删除项目初始化生成的原生style
+*/
+</style>
